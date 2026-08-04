e315c99e4fa2e8d1e7e2217f9c04327f2c00b0d1
Sambhav Jain <sambhavj2006@gmail.com>
2026-08-04 10:48:22 +0530

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import SiteShell from "@/components/shell/SiteShell";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <body id="top" className="min-h-full flex flex-col">
         <SiteShell>{children}</SiteShell>
+        <Analytics />
       </body>
     </html>
   );
